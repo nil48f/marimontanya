121af41a684f3db0c7d4d61866d2916d9e03872d
nil48f <nil48fernandez@gmail.com>
2024-04-26 17:54:32 +0200

    Cosas

--- perfil.php
+++ perfil.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cat">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mar I Montanya - Perfil</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body id="bodyPerfil">
     <?php
         require("protection.php");
     ?>
     <div id="perfilgrid">
         <div>
-            <img src="images/logo.webp" width="20%">
+            <h1><?php echo $_SESSION['usuari_nom'];?></h1>
         </div>
         <div>
             <a class="linkprofile" href="index.html">Home</a>
             <a class="linkprofile" href="logout.php">Tanca Sesio</a>
         </div>
-        <div>
-            <h1><?php echo $_SESSION['usuari_nom'];?></h1>
+        <div id="informacionPerfil">
+            <div id="imagenPerfil">
+                <img src="images/profile_photo.jpg">
+            </div>
             <p>Email: <?php echo $_SESSION['usuari_mail'];?></p>
             <p>Tel: <?php echo $_SESSION['usuari_tlf'];?></p>
             <p>Naixement: ---</p>
